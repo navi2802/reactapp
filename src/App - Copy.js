4c1602f975c7fdcecaaba9ab45a4cfdedb1182c0
unknown <taraniduttjooshi@gmail.com>
2021-06-20 01:30:29 +0530

    initial commit

--- src/App - Copy.js
+++ src/App - Copy.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi this app devloped my Naveen Joshi
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
